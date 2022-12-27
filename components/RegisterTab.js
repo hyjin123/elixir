3e9550c19615577a978ed695faa43f72609a8bd9
Sean <seanhoyeonjin@gmail.com>
2022-12-27 11:56:54 -0500

    set up navigtion and login/register functionality of the app

---
+++ components/RegisterTab.js
@@ -0,0 +1,102 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import tw from "twrnc";
+import { auth, db } from "../firebase";
+import { useNavigation } from "@react-navigation/core";
+import firebase from "firebase";
+
+const RegisterTab = ({ setLoggedInUserEmail }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("TabNavigator");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        // set the display name and the picture of the user that registers into the firestore database
+        db.collection("users")
+          .doc(user.uid)
+          .set(
+            {
+              displayName: `${firstName} ${lastName}`,
+              email: email,
+              lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+              photoURL: compressedImage.base64,
+              theme: {
+                primary: ["#cad4fc"],
+                message: ["#a8b8ff", "#bfbbf2", "#9ad8fc"],
+              },
+            },
+            { merge: true }
+          );
+
+        setLoggedInUserEmail(user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={tw`w-75`}>
+        <TextInput
+          style={tw`border-b-2 border-gray-400 text-lg bg-transparent py-2 mt-2`}
+          placeholder="First Name"
+          value={firstName}
+          onChangeText={(text) => setFirstName(text)}
+        />
+        <TextInput
+          style={tw`border-b-2 border-gray-400 text-lg bg-transparent py-2 mt-2`}
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+        />
+        <TextInput
+          style={tw`border-b-2 border-gray-400 text-lg bg-transparent py-2 mt-2`}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={tw`border-b-2 border-gray-400 text-lg bg-transparent py-2 mt-2`}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <View style={tw`justify-center items-center mt-5`}>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={tw`w-70 bg-[#fff9bb] p-4 m-1 rounded-full`}
+        >
+          <Text style={tw`text-center font-bold`}>REGISTER</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RegisterTab;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,50 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import tw from "twrnc";
+import LoginTab from "../components/LoginTab";
+import RegisterTab from "../components/RegisterTab";
+
+const LoginScreen = ({ setLoggedInUserEmail }) => {
+  const [selectedTab, setSelectedTab] = useState("register");
+
+  return (
+    <SafeAreaView style={tw`flex-1 justify-evenly items-center`}>
+      <View>
+        <Text style={tw`text-6xl tracking-widest font-light`}>Elixir</Text>
+      </View>
+      <View>
+        <View style={tw`flex-row justify-evenly items-center mb-5`}>
+          <TouchableOpacity
+            style={tw`${
+              selectedTab === "login" ? "border-b-2 border-gray-600" : ""
+            } p-3`}
+            onPress={() => setSelectedTab("login")}
+          >
+            <Text>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={tw`${
+              selectedTab === "register" && "border-b-2 border-gray-600"
+            } p-3`}
+            onPress={() => setSelectedTab("register")}
+          >
+            <Text>REGISTER</Text>
+          </TouchableOpacity>
+        </View>
+        {selectedTab === "register" ? (
+          <RegisterTab setLoggedInUserEmail={setLoggedInUserEmail} />
+        ) : (
+          <LoginTab setLoggedInUserEmail={setLoggedInUserEmail} />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
