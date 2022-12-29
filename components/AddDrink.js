0956446a0fc09938aee39c875e7b128700722b93
Sean <seanhoyeonjin@gmail.com>
2022-12-29 14:11:05 -0500

    pre-created skeletons of all the components in the home screen

---
+++ components/AddDrink.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AddDrink = () => {
+  return (
+    <View>
+      <Text>AddDrink</Text>
+    </View>
+  );
+};
+
+export default AddDrink;
---
+++ components/PreviousDrinks.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PreviousDrinks = () => {
+  return (
+    <View>
+      <Text>PreviousDrinks</Text>
+    </View>
+  );
+};
+
+export default PreviousDrinks;
