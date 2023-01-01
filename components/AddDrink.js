28dfeb7f66c13fab6f594b55a1f75cb62a430f15
Sean <seanhoyeonjin@gmail.com>
2023-01-01 12:04:34 -0500

    finished the basic UI of the what did you drink today? section

--- components/AddDrink.js
+++ components/AddDrink.js
@@ -10,43 +10,47 @@ const AddDrink = () => {
   return (
     <View style={tw`mt-15 justify-center items-center mx-5`}>
       <View>
-        <Text style={tw`text-white font-semibold text-xl`}>
+        <Text style={tw`text-white font-bold text-lg`}>
           What did you drink today?
         </Text>
       </View>
       <View style={tw`flex-row justify-center mt-3`}>
         <View style={tw`flex-1`}>
           <TouchableOpacity
-            style={tw`flex-row justify-between border-2 border-white mb-3 px-2 py-3 rounded-xl`}
+            style={tw`flex-row justify-between bg-[#282929] mb-3 px-3 py-4 rounded-xl`}
           >
-            <View style={tw`flex-row`}>
+            <View style={tw`flex-row items-center`}>
               <MaterialCommunityIcons
                 name="cup-outline"
                 size={24}
                 color="white"
               />
-              <Text style={tw`text-white`}>Normal Cup</Text>
+              <Text style={tw`text-white text-lg ml-2 font-semibold`}>
+                Normal Cup
+              </Text>
             </View>
-            <View>
+            <View style={tw`justify-center`}>
               <ChevronDownIcon color="white" />
             </View>
           </TouchableOpacity>
           <TouchableOpacity
-            style={tw`flex-row justify-between border-2 border-white px-2 py-3 rounded-xl`}
+            style={tw`flex-row justify-between bg-[#282929] px-3 py-4 rounded-xl`}
           >
-            <View style={tw`flex-row`}>
-              <Ionicons name="water-outline" size={32} color="white" />
-              <Text style={tw`text-white`}>Water</Text>
+            <View style={tw`flex-row items-center`}>
+              <Ionicons name="water-outline" size={24} color="white" />
+              <Text style={tw`text-white text-lg ml-2 font-semibold`}>
+                Water
+              </Text>
             </View>
-            <View>
+            <View style={tw`justify-center`}>
               <ChevronDownIcon color="white" />
             </View>
           </TouchableOpacity>
         </View>
         <TouchableOpacity
-          style={tw`border-2 border-white ml-3 justify-center px-2 py-3 rounded-xl`}
+          style={tw`bg-[#0099ff] ml-3 justify-center px-4 py-3 rounded-xl`}
         >
-          <Entypo name="plus" size={24} color="white" />
+          <Entypo name="plus" size={30} color="white" />
         </TouchableOpacity>
       </View>
     </View>
